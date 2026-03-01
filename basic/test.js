45bba3395399a31d77f6f864731ae811032e423e
raghubar kumar <231151@iiitt.ac.in>
2026-03-01 06:07:38 +0000

    simple table and undefined

--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,16 @@
 console.log("raghu kumar");
+const accountId = 231151
+let accountPassword = "raghu"
+var accountEmail="rag@gamil.com"
+accountType=3;
+let accn
+// here accn will be undefined
+
+// accountId=333 cannot do because const not allow change
+// for printing in tabular form 
+/*
+dont use  var  beacuse of issue scope 
+*/
+console.table([accountId,accountPassword,accountEmail,accountType,accn])
+
+console.log(accountId)
