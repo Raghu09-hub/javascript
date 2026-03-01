97dec9a94e0ae2d4cc8a50c3aeaff5c5fc5123db
raghubar kumar <231151@iiitt.ac.in>
2026-03-01 07:06:31 +0000

    diffrent conversion b/w string and number

---
+++ data_conversion/03_conversion.js
@@ -0,0 +1,37 @@
+// string, null undefined  to number
+/*
+"33"=> 33
+"33ab"=>NaN {cannot be converted}
+null=> 0 in number
+udefined => NaN cannot be converted
+*/
+
+// let str =null;
+// let inNumber =Number(str)
+// console.log(typeof inNumber)
+// console.log(inNumber)
+
+// string to boolean
+/*
+let st="a"
+let bool= Boolean(st);
+console.log(typeof bool)
+console.log(bool)
+
+" "=> fasle
+"a"=>true
+null=>false
+undefined=>false;
+*/
+
+// to string
+
+// let s=null
+// let str=String(s)
+// console.log(typeof str)
+// console.log(str)
+/*
+null => null(in string)
+undefined =>undefined(instring)
+
+*/
