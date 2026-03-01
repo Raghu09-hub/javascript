7865f575ee9b107d33b4a21b6ce4bee4f88f29e7
raghubar kumar <231151@iiitt.ac.in>
2026-03-01 06:39:42 +0000

    various data type and about null and undefined

---
+++ dataType/02_dataType.js
@@ -0,0 +1,20 @@
+"use strict";
+// nower day this are already getting treated as newer version
+// alert(3+3) we are using nodejs not browser
+
+// now dataType
+// number => 2 to power 53
+// bigint =>for large value
+// boolean =>true or false
+// string =>defined by '' or ""
+// null => standalone value
+//  undefined => when value is not given
+// symbol=> for uniquenes
+
+//  object 
+
+//  imp =>data type of null is object
+// imp => data type of undefined is undefined
+
+console.log(typeof null)
+console.log(typeof undefined)
