1ff763919314a57e956bdbeed4428078b959db01
raghubar kumar <231151@iiitt.ac.in>
2026-03-01 07:50:56 +0000

    strict check comparison null and undefined

---
+++ comparison/04_comparison.js
@@ -0,0 +1,22 @@
+// same data type no confusion
+// but nul and undefined
+
+/*
+console.log(null>0) //false
+console.log(null==0) //false
+console.log(null>=0) //true 
+//  it is basicaly inconsistent
+*/
+
+/*
+console.log(undefined>0) //false
+console.log(undefined==0) //false
+console.log(undefined>=0) //false 
+//  it is basicaly inconsistent
+*/
+
+//  for strict check with value check data type also
+// with this ===
+/*
+console.log("2"===2) will false data type is not same
+*/
